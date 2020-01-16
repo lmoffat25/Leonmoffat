f9b67ccdf7f9eea36318d5993d3cf00b222fd138
lmoffat25 <mrleonmoff@gmail.com>
2020-01-16 21:56:14 +0100

    ameliore lisibilté ancre

--- script/script.js
+++ script/script.js
@@ -22,6 +22,15 @@ $(document).ready(function(){
       });
     } // End if
   });
+
+  //Retire # des ancres
+  // remove fragment as much as it can go without adding an entry in browser history:
+  window.location.replace("#");
+
+  // slice off the remaining '#' in HTML5:
+  if (typeof window.history.replaceState == 'function') {
+    history.replaceState({}, '', window.location.href.slice(0, -1));
+  }
 });
 
 //Animation au scroll
@@ -35,12 +44,3 @@ $(document).ready(function(){
 
 //Parallax
   // $('.bgsites').parallax({imageSrc: './media/parallax.jpg'});
-
-//Retire # des ancres
-// remove fragment as much as it can go without adding an entry in browser history:
-window.location.replace("#");
-
-// slice off the remaining '#' in HTML5:
-if (typeof window.history.replaceState == 'function') {
-  history.replaceState({}, '', window.location.href.slice(0, -1));
-}
