39f6f1bd98b5a5ef22561b368e9b459bc41ca2e6
lmoffat25 <mrleonmoff@gmail.com>
2020-01-16 21:54:22 +0100

    ameliore lisibilté ancre

--- script/script.js
+++ script/script.js
@@ -36,3 +36,11 @@ $(document).ready(function(){
 //Parallax
   // $('.bgsites').parallax({imageSrc: './media/parallax.jpg'});
 
+//Retire # des ancres
+// remove fragment as much as it can go without adding an entry in browser history:
+window.location.replace("#");
+
+// slice off the remaining '#' in HTML5:
+if (typeof window.history.replaceState == 'function') {
+  history.replaceState({}, '', window.location.href.slice(0, -1));
+}
