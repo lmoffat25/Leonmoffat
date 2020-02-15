89b6a4fec9543ab510e546e9bb5a127a2d6a337b
lmoffat25 <mrleonmoff@gmail.com>
2020-02-15 14:14:17 +0100

    modifs script

--- script/script.js
+++ script/script.js
@@ -69,9 +69,9 @@ $
 //Animation au scroll
   AOS.init({
     delay: 200, // values from 0 to 3000, with step 50ms
-  duration: 1600, // values from 0 to 3000, with step 50ms
-  easing: 'ease', // default easing for AOS animations
-  once: true, // whether animation should happen only once - while scrolling down
+    duration: 1600, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: true, // whether animation should happen only once - while scrolling down
   });
 
 
